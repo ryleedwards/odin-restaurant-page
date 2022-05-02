9a045a1422bce01416c40763791cdb0eacd20d0a
Ryle Edwards <ryle.edwards@gmail.com>
2022-05-02 16:33:10 -0500

    Fix fontawesome icons for insta and fb

--- src/navbar.js
+++ src/navbar.js
@@ -1,7 +1,12 @@
 import createElement from "./util/createElement";
 
+//create navbar
 const navbar = createElement("header", "header");
+
+// logo
 const logo = createElement("div", "nav-logo", ["navbar"], "The Blue Cat");
+
+// list of links
 const navList = createElement("ul", "", ["nav-list"]);
 const navLinkBuilders = ["home", "menu", "contact"];
 const navLinks = [];
@@ -12,7 +17,25 @@ for (let i = 0; i < navLinkBuilders.length; i++) {
 navLinks.forEach((element) => {
   navList.appendChild(element);
 });
+
+// brand icons + social
+const facebook = createElement("i", "facebook-icon", [
+  "icons",
+  "fa-brands",
+  "fa-facebook",
+]);
+const instagram = createElement("i", "instagram-icon", [
+  "icons",
+  "fa-brands",
+  "fa-instagram",
+]);
+
+const iconContainer = createElement("div", "icon-container");
+iconContainer.appendChild(facebook);
+iconContainer.appendChild(instagram);
+
 navbar.appendChild(logo);
 navbar.appendChild(navList);
+navbar.appendChild(iconContainer);
 
 export { navbar };

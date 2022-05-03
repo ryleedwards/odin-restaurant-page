c18e1cd65d945e82efd6f2443375f8a264230fc1
Ryle Edwards <ryle.edwards@gmail.com>
2022-05-03 08:22:58 -0500

    Swap out for loop to forEach in navbar

--- src/navbar.js
+++ src/navbar.js
@@ -10,10 +10,11 @@ const logo = createElement("div", "nav-logo", ["navbar"], "The Blue Cat");
 const navList = createElement("ul", "", ["nav-list"]);
 const navLinkBuilders = ["home", "menu", "contact"];
 const navLinks = [];
-for (let i = 0; i < navLinkBuilders.length; i++) {
-  let name = navLinkBuilders[i];
-  navLinks[i] = createElement("li", `link-${name}`, ["nav-links"], name);
-}
+
+navLinkBuilders.forEach((link) => {
+  navLinks.push(createElement("li", `link-${link}`, ["nav-links"], link));
+});
+
 navLinks.forEach((element) => {
   navList.appendChild(element);
 });

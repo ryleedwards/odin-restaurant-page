dae712f1842969f8f228bfe3f53682ad833bc4b1
Ryle Edwards <ryle.edwards@gmail.com>
2022-05-03 12:43:59 -0500

    Refactor navbar.js to utilize and export function, not element

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import "./style.css";
 import createElement from "./util/createElement";
-import { navbar } from "./navbar";
+import { createNavbar } from "./navbar";
 import { createHome } from "./home";
 
 const content = document.getElementById("content");
-content.appendChild(navbar);
+content.appendChild(createNavbar());
 content.appendChild(createHome());
--- src/navbar.js
+++ src/navbar.js
@@ -1,46 +1,50 @@
 import createElement from "./util/createElement";
 
-//create navbar
-const navbar = createElement("header", "header");
+function createNavbar() {
+  //create navbar
+  const navbar = createElement("header", "header");
 
-// logo
-const logo = createElement("div", "nav-logo", "navbar", "The Blue Cat Lodge");
+  // logo
+  const logo = createElement("div", "nav-logo", "navbar", "The Blue Cat Lodge");
 
-// list of links
-const navList = createElement("ul", "", ["nav-list"]);
-const navLinkBuilders = ["home", "menu", "contact"];
-const navLinks = [];
+  // list of links
+  const navList = createElement("ul", "", ["nav-list"]);
+  const navLinkBuilders = ["home", "menu", "contact"];
+  const navLinks = [];
 
-navLinkBuilders.forEach((link) => {
-  let li = createElement("li", `link-${link}`, "nav-item");
-  let a = createElement("a", `a-${link}`, "nav-link", link);
-  a.setAttribute("href", "#");
-  li.appendChild(a);
-  navLinks.push(li);
-});
+  navLinkBuilders.forEach((link) => {
+    let li = createElement("li", `link-${link}`, "nav-item");
+    let a = createElement("a", `a-${link}`, "nav-link", link);
+    a.setAttribute("href", "#");
+    li.appendChild(a);
+    navLinks.push(li);
+  });
 
-navLinks.forEach((element) => {
-  navList.appendChild(element);
-});
+  navLinks.forEach((element) => {
+    navList.appendChild(element);
+  });
 
-// brand icons + social
-const facebook = createElement(
-  "i",
-  "facebook-icon",
-  "icons fa-brands fa-facebook"
-);
-const instagram = createElement(
-  "i",
-  "instagram-icon",
-  "icons fa-brands fa-instagram"
-);
+  // brand icons + social
+  const facebook = createElement(
+    "i",
+    "facebook-icon",
+    "icons fa-brands fa-facebook"
+  );
+  const instagram = createElement(
+    "i",
+    "instagram-icon",
+    "icons fa-brands fa-instagram"
+  );
 
-const iconContainer = createElement("div", "icon-container");
-iconContainer.appendChild(facebook);
-iconContainer.appendChild(instagram);
+  const iconContainer = createElement("div", "icon-container");
+  iconContainer.appendChild(facebook);
+  iconContainer.appendChild(instagram);
 
-navbar.appendChild(logo);
-navbar.appendChild(navList);
-navbar.appendChild(iconContainer);
+  navbar.appendChild(logo);
+  navbar.appendChild(navList);
+  navbar.appendChild(iconContainer);
 
-export { navbar };
+  return navbar;
+}
+
+export { createNavbar };

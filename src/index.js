aa12c56f8eccbae18853c221b310a55af884da8e
Ryle Edwards <ryle.edwards@gmail.com>
2022-05-03 14:33:53 -0500

    Apply logic to clicking home btn in navbar

--- src/index.js
+++ src/index.js
@@ -5,4 +5,28 @@ import { createHome } from "./home";
 
 const content = document.getElementById("content");
 content.appendChild(createNavbar());
+const navs = document.querySelectorAll(".nav-link");
+console.log(navs);
+navs.forEach((a) => {
+  a.addEventListener("click", (e) => {
+    navigateTo(e.target);
+  });
+});
 content.appendChild(createHome());
+
+function navigateTo(destination) {
+  //remove existing content on the page
+  content.removeChild(content.childNodes[1]);
+  //check which nav was clicked and populate page
+  if (destination.id == "a-home") {
+    content.appendChild(createHome());
+    destination.classList.toggle("disabled");
+  }
+
+  //enable the other destination links
+  navs.forEach((a) => {
+    if (a !== destination) {
+      a.classList.remove("disabled");
+    }
+  });
+}
